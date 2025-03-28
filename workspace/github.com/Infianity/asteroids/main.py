305a211896452645f2b16eea5069a851db2b1fce
Infianity <ianseideman@gmail.com>
2025-03-27 22:18:00 -0400

    Clean up main.py

--- workspace/github.com/Infianity/asteroids/main.py
+++ workspace/github.com/Infianity/asteroids/main.py
@@ -9,16 +9,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock() #create a clock object
+    dt = 0 #delta time``
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        
+        screen.fill("black")
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
